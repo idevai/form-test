d55178a81a7eeb8af8bf3e0e3e8ba402f87439be
U-UPCIT\istdevai <istdevai@ATWS001624.upcit.ds.upc.biz>
2017-04-04 16:37:00 +0200

    update

--- form-row.js
+++ form-row.js
@@ -1,3 +1,23 @@
-/**
- * Created by istdevai on 04.04.2017.
- */
+function animateInlineLabel(element, currentClass, targetClass) {
+    // var placeholder = element.parent().find('.form-component-placeholder');
+    // element.switchClass(currentClass, targetClass, 300);
+    element.removeClass(currentClass).addClass(targetClass);
+}
+
+$(document).ready(function () {
+    // $('.form-component input').each(function () {
+    //     if ($(this).val()) {
+    //         $(this).addClass('shrunk');
+    //     }
+    // });
+    $('body').on('focus', '.form-row input', function () {
+        animateInlineLabel($(this).parent(), 'label-expanded', 'label-shrunk');
+    });
+    $('body').on('blur', '.form-row input', function () {
+        if (!$(this).val()) {
+            animateInlineLabel($(this).parent(), 'label-shrunk', 'label-expanded');
+        }
+    });
+});
+
+
